ff3a238b76ac1f569116b3e5f37e90c84da13976
bob <eduards.br7@gmail.com>
2025-12-02 18:29:13 +0100

    signal-proccessing-attempt

--- embedded.py
+++ embedded.py
@@ -1,5 +1,6 @@
 #import qcm_data_collection
 import time
+import collections
 
 class control:
     PUMP_GPIO=1
@@ -23,8 +24,8 @@ class control:
         self.__start_pump()
 
         sample_sums=0
-        for _ in len(range(self.__FREQUENCY_SAMPLE_SIZE)):
-            sample_sums+=qcm.get_qcm_frequency()
+        for _ in range(self.__FREQUENCY_SAMPLE_SIZE):
+            sample_sums += qcm.get_qcm_frequency()
             time.sleep(self.__SECONDS_BETWEEN_SAMPLES)
 
         self.__stop_pump()
@@ -37,16 +38,86 @@ class control:
     
 # sudo apt update
 # sudo apt install python3-dev python3-pip python3-setuptools
-# pip3 install RPi.GPIO -GPIO can only be installed on a Raspberry Pi OS.
-# used to get data from the raspb.Pi pin.
+# pip3 install RPi.GPIO - GPIO can only be installed on a Raspberry Pi OS.
+# used to get data from the QCM pin.
 class qcm:
-    @staticmethod
-    def get_qcm_frequency():
-        import RPi.GPIO as GPIO
-        QCM_PIN = 3  # Change to the actual QCM sensor pin we're using
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(QCM_PIN, GPIO.IN)
-        # Read the sensor value
-        value = GPIO.input(QCM_PIN)
-        # we likely need to process 'value' to get the actual data, not sure how the sensor input looks like
-        return value
+    """QCM input helper with simple denoising and frequency estimation.
+
+    Reads a digital input pin (or simulates one if RPi.GPIO isn't available),
+    counts rising edges over a short window to estimate frequency (Hz), and
+    applies moving-average + exponential moving-average smoothing.
+    """
+
+    QCM_PIN = 3
+    _gpio_initialized = False
+    _use_gpio = True
+    QCM_MEASURE_WINDOW = 0.05  # seconds per internal measurement window
+    DEBOUNCE_TIME = 0.0005    # ignore changes shorter than this (seconds)
+    SMOOTHING_WINDOW = 5
+    _ma_buffer = collections.deque(maxlen=SMOOTHING_WINDOW)
+    _ema_alpha = 0.3
+    _ema = None
+
+    @classmethod
+    def _init_gpio(cls):
+        try:
+            import RPi.GPIO as GPIO
+            cls.GPIO = GPIO
+            cls.GPIO.setwarnings(False)
+            cls.GPIO.setmode(cls.GPIO.BCM)
+            cls.GPIO.setup(cls.QCM_PIN, cls.GPIO.IN)
+            cls._gpio_initialized = True
+            cls._use_gpio = True
+        except Exception:
+            # Fall back to a simulator when not running on Raspberry Pi
+            cls.GPIO = None
+            cls._gpio_initialized = True
+            cls._use_gpio = False
+
+    @classmethod
+    def _read_raw(cls):
+        """Return raw digital input from the pin (0 or 1)."""
+        if not cls._gpio_initialized:
+            cls._init_gpio()
+        if cls._use_gpio and cls.GPIO is not None:
+            return cls.GPIO.input(cls.QCM_PIN)
+        # Simulator: pseudo-square wave for local testing (~10 Hz when polled fast)
+        return int((time.time() * 20) % 2)
+
+    @classmethod
+    def get_qcm_frequency(cls):
+        """Estimate frequency in Hz by counting rising edges.
+
+        Uses a short polling window and debouncing to avoid counting noise,
+        then smooths measurements using moving average and EMA.
+        """
+        end_time = time.time() + cls.QCM_MEASURE_WINDOW
+        last = cls._read_raw()
+        last_change_time = time.time()
+        count = 0
+
+        while time.time() < end_time:
+            cur = cls._read_raw()
+            if cur != last:
+                now = time.time()
+                if now - last_change_time >= cls.DEBOUNCE_TIME:
+                    # count rising edges only
+                    if last == 0 and cur == 1:
+                        count += 1
+                    last_change_time = now
+                    last = cur
+            time.sleep(0.0005)
+
+        frequency = count / cls.QCM_MEASURE_WINDOW
+
+        # moving average buffer
+        cls._ma_buffer.append(frequency)
+        ma = sum(cls._ma_buffer) / len(cls._ma_buffer)
+
+        # exponential moving average
+        if cls._ema is None:
+            cls._ema = ma
+        else:
+            cls._ema = cls._ema_alpha * ma + (1 - cls._ema_alpha) * cls._ema
+
+        return cls._ema
