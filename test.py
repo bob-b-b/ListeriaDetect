ad62a4f1e0cf29d97cdcba0220eec37a9587dc24
Connie Palouda <japal23@student.sdu.dk>
2026-01-14 13:06:31 +0100

    initialization 2

---
+++ test.py
@@ -0,0 +1,99 @@
+import pyqtgraph as pg
+from PyQt5 import QtCore, QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, time_range):
+        super().__init__()
+
+        #Text Screen
+        self.textscreen = QtWidgets.QLabel(self)
+        self.textscreen.hide()
+        self.textscreen.setWindowFlags(QtCore.Qt.CustomizeWindowHint)
+        font = self.textscreen.font()
+        font.setPixelSize(30)
+        self.textscreen.setFont(font)
+        self.textscreen.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+
+        self.setCentralWidget(self.textscreen)
+
+        #Frequency Plot
+        self.plot_graph = pg.PlotWidget()
+        self.plot_graph.hide()
+        self.plot_graph.setWindowFlags(QtCore.Qt.CustomizeWindowHint)
+        self.plot_graph.setTitle("Frequency Over Time", color="w", size="20pt") #MAKE TITLE CHANGEABLE IN A METHOD
+        styles = {"color": "red", "font-size": "18px"}
+        self.plot_graph.setLabel("left", "Frequency (unit)", **styles)
+        self.plot_graph.setLabel("bottom", "Time (unit)", **styles)
+        self.plot_graph.addLegend()
+        self.plot_graph.showGrid(x=True, y=True)
+        self.plot_graph.setXRange(0, 10)
+        #self.plot_graph.setYRange(0, 10)
+
+        self.time = time_range
+        self.buffer = []
+        self.sample = []
+
+        pen = pg.mkPen(color=(0, 0, 255))
+        self.bufferline = self.plot_graph.plot(
+            range(1, len(self.buffer) + 1),
+            self.buffer,
+            name="Buffer",
+            pen=pen,
+        )
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.sampleline = self.plot_graph.plot(
+            range(1, len(self.sample) + 1),
+            self.sample,
+            name="Sample",
+            pen=pen,
+        )
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(500)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
+
+
+#These methods update the graph itself
+    def update_plot(self):
+        self.bufferline.setData(range(1, len(self.buffer) + 1), self.buffer)
+        self.sampleline.setData(range(1, len(self.sample) + 1), self.sample)
+
+    def set_title(self, title):
+        self.plot_graph.setTitle(title, color="w", size="20pt")
+
+
+#These methods update the data used in the graph
+    def add_value_buffer(self,value):
+        if len(self.buffer) < self.time:
+            self.buffer.append(value) #cut off values after time_range
+
+    def add_value_sample(self,value):
+        if len(self.sample) < self.time:
+            self.sample.append(value) #cut off values after time_range
+
+    def clear_data(self):
+        self.buffer = []
+        self.sample = []
+
+
+#These methods switch between showing graph and instruction
+    def show_text(self, text):
+        self.textscreen.setText(text)
+        self.plot_graph.hide()
+        self.textscreen.showMaximized()
+
+    def show_graph(self):
+        self.textscreen.hide()
+        self.plot_graph.showMaximized()
+    
+
+app = QtWidgets.QApplication([])
+main = MainWindow(10)
+main.setWindowFlags(QtCore.Qt.CustomizeWindowHint)
+main.showMaximized()
+main.show_graph()
+main.show_text("hello")
+main.show_graph()
+app.exec()
